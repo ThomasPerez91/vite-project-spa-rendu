4f05d53f4992bc6ee025eadaced14083a0a5d897
Thomas PEREZ <92369255+DiallThydus@users.noreply.github.com>
2022-12-22 15:11:51 +0100

    feat: add navbar (#3)
    Co-authored-by: Diall Thydus <pro.thomas.perez@gmail.com>
    Co-authored-by: sonny-ipssi <s.lallier@ecole-ipssi.net>

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 import PageCharacter from './src/pages/PageCharacter';
 import TabManager from './src/utils/TabManager';
+import NavBar from "./src/components/NavBar.js";
 
 import './src/styles/style.css';
+import './src/styles/navbar.css';
+import './src/styles/character.css';
 
+const headerElement = document.querySelector('header');
+headerElement.appendChild(NavBar());
+const footerElement = document.querySelector('footer');
 const rootElement = document.querySelector('#app');
 
 const tabManager = new TabManager(rootElement, {
