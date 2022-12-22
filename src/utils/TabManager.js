29f5954ec8acca7ea4e1c9efc7727553058d624a
sonny-ipssi <s.lallier@ecole-ipssi.net>
2022-12-22 12:33:25 +0100

    prettier

--- src/utils/TabManager.js
+++ src/utils/TabManager.js
@@ -1,19 +1,20 @@
 class TabManager {
-  constructor(rootElement, componentMapping) {
-    this.rootElement = rootElement
-    this.componentMapping = componentMapping
-  }
-
-  async openTabById(id) {
-    if (id in this.componentMapping) {
-      const { component, params: [...props] } = this.componentMapping[id]
-      const Component = await component(...props)
-      this.rootElement.innerHTML = ''
-      this.rootElement.appendChild(Component)
-    } else {
-      console.error('Invalid id provided')
+    constructor(rootElement, componentMapping) {
+        this.rootElement = rootElement;
+        this.componentMapping = componentMapping;
     }
-  }
+
+    async openTabById(id) {
+        if (id in this.componentMapping) {
+            console.log(this.componentMapping[id])
+            const { component, params: [...props] } = this.componentMapping[id];
+            const Component = await component(...props);
+            this.rootElement.innerHTML = '';
+            this.rootElement.appendChild(Component);
+        } else {
+            console.error('Invalid id provided');
+        }
+    }
 }
 
-export default TabManager
+export default TabManager;
