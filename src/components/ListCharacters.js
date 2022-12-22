b5d94b0e0dd0bd31a24281aa2051e77f19f96aae
sonny-ipssi <s.lallier@ecole-ipssi.net>
2022-12-22 13:01:58 +0100

    remove html template & replace document.createElement

--- src/components/ListCharacters.js
+++ src/components/ListCharacters.js
@@ -1,13 +1,12 @@
+import createElement from "../utils/createElement";
 import { Character } from "./Character";
 
 export default function ListCharacters(characters) {
-    const template = document.querySelector('#listOfElement');
-    const element = template.content.cloneNode(true);
-    console.log(characters);
-
-    characters.forEach((character) => {
-        element.querySelector('div').appendChild(Character(character));
+    const template = createElement({
+        tagName: 'div',
+        classList: ['character-page']
     });
+    characters.forEach((character) => template.append(Character(character)));
 
-    return element;
+    return template;
 }
